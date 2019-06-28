841e4baa07ae42153b78c95f153c7a47311be30a
马聪 <jon_markov@sina.com>
2019-06-28 18:14:52 +0800

    骨架搭建完毕

---
+++ src/pages/index/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+  import {get} from '@/utils/http.js'
+
+  export default {
+    data () {
+      return {
+        category: {}
+      }
+    },
+    mounted () {
+      this.getCategory()
+    },
+    methods: {
+      async getCategory () {
+        const data = await get('/shop/goods/category/all')
+        this.category = data.data
+        console.log(data)
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/utils/http.js
@@ -0,0 +1,38 @@
+// 请求的封装
+const host = 'https://api.it120.cc/wds'
+export {
+  host
+}
+// 请求封装
+function request (url, method, data, header = {}) {
+  wx.showLoading({
+    title: '加载中' // 数据请求前loading
+  })
+  return new Promise((resolve, reject) => {
+    wx.request({
+      url: host + url, // 仅为示例，并非真实的接口地址
+      method: method,
+      data: data,
+      header: {
+        'content-type': 'application/json' // 默认值
+      },
+      success: function (res) {
+        wx.hideLoading()
+        resolve(res.data)
+      },
+      fail: function (err) {
+        wx.hideLoading()
+        reject(err, false)
+      },
+      complete: function () {
+        wx.hideLoading()
+      }
+    })
+  })
+}
+export function get (url, data) {
+  return request(url, 'GET', data)
+}
+export function post (url, data) {
+  return request(url, 'POST', data)
+}
