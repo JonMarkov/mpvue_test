aabf8a4d185efab9e8b9e2ba7577bf6221c8719f
马聪 <jon_markov@sina.com>
2019-06-28 19:50:08 +0800

    带参数请求示例，post and get

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,12 +1,11 @@
 <template>
   <div>
-
+    <div v-for="(item, index) in category" :key="index">{{index+1}} {{item.name}}</div>
   </div>
 </template>
 
 <script>
   import {get} from '@/utils/http.js'
-
   export default {
     data () {
       return {
@@ -14,12 +13,20 @@
       }
     },
     mounted () {
+      // 函数执行 请求接口
       this.getCategory()
     },
     methods: {
+      // 函数定义 请求接口
       async getCategory () {
-        const data = await get('/shop/goods/category/all')
+        // 封装参数集合
+        let params = {}
+        const newparams = Object.assign(params)
+        // 开始执行封装的请求
+        const data = await get('/shop/goods/category/all', newparams)
+        // 把返回的数据渲染到数据data中
         this.category = data.data
+        // 打印出返回的数据
         console.log(data)
       }
     }
